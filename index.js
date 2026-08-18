c0f845aefce26cf9e9f2b3eaac339fbcbb6f14ca
jmedal14-lab <jmedal14@gmail.com>
2026-08-18 13:06:18 -0400

    final push, all complete

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// template_29fowbi
-// service_dggs3zw
-// HvFD2z8yh10r5WnL0
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const booleanInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * booleanInt}px, ${y * booleanInt}px )`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -27,12 +48,12 @@ function contact(event) {
       );
     });
 }
-let isModalOpen = false;
+
 function toggleModal() {
-  if(isModalOpen) {
-    isModalOpen= false;
-    return document.body.classList.remove('modal--open');
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
   }
   isModalOpen = true;
-  document.body.classList += " modal--open"
+  document.body.classList += " modal--open";
 }
